9435426504b181b3eda7cb56feb064be287143b2
RohanRaj928 <100634190+RohanRaj928@users.noreply.github.com>
2025-08-12 21:07:53 +0100

    Finished train.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from newspaper import Article
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 import torch
 
 
@@ -8,21 +7,3 @@ article = Article(url)
 article.download()
 article.parse()
 
-# Load tokenizer (converts text to model input)
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-
-# Load pretrained BERT for Classification with 2 classes (change if needed)
-model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=3)
-
-# Tokenize the text, convert to IDs, create attention masks
-inputs = tokenizer(article.text, return_tensors="pt", truncation=True, padding=True)
-
-outputs = model(**inputs)
-
-# Get logits (raw scores before softmax)
-logits = outputs.logits
-
-# Convert logits to predicted class
-predicted_class = torch.argmax(logits, dim=1).item()
-
-print(f"Predicted class: {predicted_class}")
--- src/train.py
+++ src/train.py
@@ -17,15 +17,16 @@ class Dataset(torch.utils.data.Dataset):
 
 def main():
 
-    import pathlib
-
+    import pathlib, os
     import pandas as pd
+    import torch
     from torch.utils.data import DataLoader
     from transformers import LongformerTokenizerFast, LongformerForSequenceClassification
-    import torch
     import tqdm  # Progress Bar
 
-    # Training Variables Default
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+    ## Training Variables Default ##
     num_epochs = 3
     batch_size = 2
     learning_rate = 1e-5
@@ -46,48 +47,72 @@ def main():
 
     print("Using device:", device.type)
 
+    ## Importing Dataset ##
 
     print("Importing Dataset...")
     splits = {'train': 'data/train-00000-of-00001.parquet', 'test': 'data/test-00000-of-00001.parquet', 'valid': 'data/valid-00000-of-00001.parquet'}
-    df = pd.read_parquet("hf://datasets/siddharthmb/article-bias-prediction-media-splits/" + splits["train"])
+    df_train = pd.read_parquet("hf://datasets/siddharthmb/article-bias-prediction-media-splits/" + splits["train"])
+    df_valid = pd.read_parquet("hf://datasets/siddharthmb/article-bias-prediction-media-splits/" + splits["valid"])
 
+    ## Preparing Dataset for Training ##
 
     print("Preparing for training...")
     # 0: Left, 1: Right , 2: Center
-    text = df['content'].tolist()
-    labels = df['bias_text'].tolist()
+    text_train = df_train['content'].tolist()
+    labels_train = df_train['bias_text'].tolist()
+
+    text_valid = df_valid['content'].tolist()
+    labels_valid = df_valid['bias_text'].tolist()
 
 
     #Load Tokenizer
     tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096')
-    encodings = tokenizer(text, padding=True, truncation=True, max_length=4096, return_tensors='pt') # Tokenize text
-    dataset = Dataset(encodings, labels)
+
+    # Encode text
+    encodings = tokenizer(text_train, padding=True, truncation=True, max_length=4096, return_tensors='pt') # Tokenize text
+    dataset_train = Dataset(encodings, labels_train)
+
+    encodings = tokenizer(text_valid, padding=True, truncation=True, max_length=4096, return_tensors='pt')
+    dataset_valid = Dataset(encodings, labels_valid)
+
+    # Create Dataloaders
+    train_loader = DataLoader(dataset_train, batch_size, shuffle=True, pin_memory=True, num_workers=num_workers)
+    valid_loader = DataLoader(dataset_valid, batch_size, shuffle=False, pin_memory=True, num_workers=num_workers)
 
     #Load Model
     model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096', num_labels=3)
     model.to(device)
 
-    # Save the model
-    print(f"Saving model to {filepath}...")
-    filepath.mkdir(parents=True, exist_ok=True)
-    model.save_pretrained(filepath)
-    tokenizer.save_pretrained(filepath)
-    print("Model saved.")
-
-    # Create DataLoader
-    train_loader = DataLoader(dataset, batch_size, shuffle=True, pin_memory=True, num_workers=num_workers)
-
 
     # Optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-    # Training Loop
+    ## Training ##
+
+    # Function to evaluate validation loss
+    def evaluate(model, dataloader, device):
+        model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for batch in dataloader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                outputs = model(**batch)
+                loss = outputs.loss
+                total_loss +=  loss.item()
+
+        avg_loss = total_loss / len(dataloader)
+        return avg_loss
+
+
+
     print("Training...")
+
     model.train()
 
     if device.type == 'cuda':
         scaler = torch.cuda.amp.GradScaler()
 
+    # Training Loop
     for epoch in range(num_epochs):
         print(f"Epoch {epoch+1}/{num_epochs}")
         total_loss = 0
@@ -124,8 +149,13 @@ def main():
                 loss.backward()
                 optimizer.step()
 
-        avg_loss = total_loss / len(train_loader)
-        print(f"Average Loss for Epoch {epoch + 1}: {avg_loss:.4f}\n")
+        avg_train_loss = total_loss / len(train_loader)
+        print(f"Average Training Loss for Epoch {epoch + 1}: {avg_train_loss:.4f}\n")
+
+        avg_valid_loss = evaluate(model, valid_loader, device)
+        print(f"Average Validation Loss for Epoch {epoch + 1}: {avg_valid_loss:.4f}\n")
+
+
 
 
     # Save the model
